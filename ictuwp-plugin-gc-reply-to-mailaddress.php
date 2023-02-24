24e746dce5ab98be35b78b4fba77e4366d5253d1
Paul van Buuren <paul@wbvb.nl>
2023-02-24 11:45:35 +0100

    create plugin file

---
+++ ictuwp-plugin-gc-reply-to-mailaddress.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @link                https://github.com/ICTU/ictuwp-plugin-gc-reply-to-mailaddress
+ * @package             ictuwp-plugin-gc-reply-to-mailaddress
+ *
+ * @wordpress-plugin
+ * Plugin Name:         ICTU / Gebruiker Centraal / Set reply-to emailaddress
+ * Plugin URI:          https://github.com/ICTU/ictuwp-plugin-gc-reply-to-mailaddress
+ * Description:         Plugin om het antwoord-adres voor e-mail te corrigeren
+ * Version:             0.0.1
+ * Version description: Fix thema_sort_order
+ * Author:              Paul van Buuren
+ * Author URI:          https://github.com/paulvanbuuren
+ * License:             GPL-2.0+
+ * License URI:         http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:         gctheme
+ * Domain Path:         /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
